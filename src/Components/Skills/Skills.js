939a9bad7054b9dff34de50d8d03a0bca31603c3
Kavinkumar1998 <kavinajith412@gmail.com>
2023-07-03 15:41:41 +0530

    changes added

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -17,6 +17,7 @@ import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import { themeContext } from '../../Context';
 import { useContext } from "react";
+import { LinearProgress } from "@mui/material";
 const Skills = () => {
     const theme=useContext(themeContext);
   const darkMode=theme.state.darkMode;
@@ -33,72 +34,20 @@ const Skills = () => {
         Skills And Tools
         </h1>
         <div className="Techs">
-        <img data-aos="flip-up"data-aos-delay="600" src={js} alt="javascript" />
-         <img data-aos="flip-up"data-aos-delay="600" src={Html} alt="Html" />
-         <img data-aos="flip-up"data-aos-delay="600" src={Css} alt="Css" />
-         <img data-aos="flip-up"data-aos-delay="600" src={ReactSkill} alt="react" />
-         <img data-aos="flip-up"data-aos-delay="600" src={Redux} alt="Redux" />
-         <img data-aos="flip-up"data-aos-delay="600" src={mui} alt="Mui" />
-         <img data-aos="flip-down"data-aos-delay="600" src={bootstrap} alt="bootstrap" />
-         <img data-aos="flip-down"data-aos-delay="600" src={Node} alt="Node" />
-         <img data-aos="flip-down"data-aos-delay="600" src={Mongo} alt="Mongo" />
-         <img data-aos="flip-down"data-aos-delay="600" src={Github} alt="github" />
-         <img data-aos="flip-down"data-aos-delay="600" src={Git} alt="Git" />
-         <img data-aos="flip-down"data-aos-delay="600" src={aws} alt="aws" />
-        </div>
+<div data-aos="flip-up"data-aos-delay="600" className="skilllist"> <img  className="img-skill" src={js} alt="javascript" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={80} /></div>
+<div data-aos="flip-up"data-aos-delay="600" className="skilllist"> <img  className="img-skill" src={Html} alt="Html" /><LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={80} /> </div>
+<div data-aos="flip-up"data-aos-delay="600" className="skilllist"><img  className="img-skill" src={Css} alt="Css" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={80} /></div>
+<div data-aos="flip-up"data-aos-delay="600" className="skilllist"><img  className="img-skill" src={ReactSkill} alt="react" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={80} /></div>
+<div data-aos="flip-up"data-aos-delay="600" className="skilllist"> <img  className="img-skill" src={Redux} alt="Redux" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={60} /></div>
+<div data-aos="flip-up"data-aos-delay="600" className="skilllist"> <img  className="img-skill" src={mui} alt="Mui" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={80} /></div>
+<div data-aos="flip-down"data-aos-delay="600" className="skilllist"> <img  className="img-skill" src={bootstrap} alt="bootstrap" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={75} /></div>
+<div data-aos="flip-down"data-aos-delay="600" className="skilllist"> <img  className="img-skill" src={Node} alt="Node" /><LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={80} /></div>
+<div data-aos="flip-down"data-aos-delay="600" className="skilllist"> <img  className="img-skill" src={Mongo} alt="Mongo" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={70} /></div>
+<div data-aos="flip-down"data-aos-delay="600" className="skilllist"><img  className="img-skill" src={Github} alt="github" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={80} /></div>     
+<div data-aos="flip-down"data-aos-delay="600" className="skilllist"> <img  className="img-skill" src={Git} alt="Git" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={60} /></div>
+<div data-aos="flip-down"data-aos-delay="600" className="skilllist"> <img  className="img-skill" src={aws} alt="aws" /> <LinearProgress sx={{width:"7rem",marginLeft:"10px"}} color="success" variant="determinate" value={50} /></div>      
+             </div>
 
-
-
-     
-{/* 
-            <div className="skill-box">
-                <span className="title">HTML</span>
-                <div className="skill-bar">
-                    <span className="skill-per html">
-                        <span className="tooltip">95%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">CSS</span>
-                <div className="skill-bar">
-                    <span className="skill-per css">
-                        <span className="tooltip">80%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">JavaScript</span>
-                <div className="skill-bar">
-                    <span className="skill-per javascript">
-                        <span className="tooltip">60%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">NodeJS</span>
-                <div className="skill-bar">
-                    <span className="skill-per nodejs">
-                        <span className="tooltip">40%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">ReactJS</span>
-                <div className="skill-bar">
-                    <span className="skill-per reactjs">
-                        <span className="tooltip">70%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">ExpressJS</span>
-                <div className="skill-bar">
-                    <span className="skill-per expressjs">
-                        <span className="tooltip">75%</span>
-                    </span>
-                </div>
-            </div> */}
         </div>
       
     
